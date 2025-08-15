43a289bf4a35b1c0797b20dd6574a864a9d8d0cc
Nazmul Islam <121036537+Nazmul0005@users.noreply.github.com>
2025-08-15 17:37:32 +0600

    Update model.py

--- model.py
+++ model.py
@@ -3,5 +3,9 @@ from transformers import pipeline
 
 classifier = pipeline(
     "text-classification",
-    model="unitary/toxic-bert"
+    model="unitary/toxic-bert" 
+    #small model : Model	Size	Architecture
+    #unitary/toxic-bert	~438MB	BERT-base
+    #martin-ha/toxic-comment-model	~267MB	DistilBERT
+    #s-nlp/roberta_toxicity_classifier	~150MB	Mini RoBERTa
 )
